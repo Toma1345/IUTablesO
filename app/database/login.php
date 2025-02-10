0d181df48484dca8fa3b5ed09ad5d3e47e14b696
Toma1345 <thomas.brossier@etu.univ-orleans.fr>
2025-02-10 09:08:36 +0100

    maj login

--- app/database/login.php
+++ app/database/login.php
@@ -8,8 +8,8 @@ use PHPSupabase\Auth;
 use PHPSupabase\Service;
 
 // Paramètres de l'API Supabase
-$apiKey = 'VOTRE_CLE_API'; // Remplacez par votre clé API
-$apiUrl = 'https://votre-projet.supabase.co'; // Remplacez par l'URL de votre projet
+$apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpb3V5cWl3d29pbWp3ZGNsdHJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczODUyNzgsImV4cCI6MjA1Mjk2MTI3OH0.g23MjTRR9h86LJKphD2D74geh0K1ETeJCjk65UWIwhY'; // Remplacez par votre clé API
+$apiUrl = 'https://tiouyqiwwoimjwdcltrl.supabase.co';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'] ?? '';
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if(isset($data->access_token)){
             $userData = $data->user; //get the user data
             echo 'Login successfully for user ' . $userData->email;
-
+        }
     } catch(Exception $e){
         echo $auth->getError();
         // Gestion des erreurs
